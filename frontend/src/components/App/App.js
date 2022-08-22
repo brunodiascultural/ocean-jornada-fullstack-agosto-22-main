de534d83fee262a23724435b11773bd4a8b2f63f
brunodiascultural <brunodiascultural@gmail.com>
2022-08-22 11:46:06 -0300

    Componente Jogo criado

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,10 +1,11 @@
 
+import Jogo from '../Jogo/Jogo';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Jogo />
     </div>
   );
 }
---
+++ frontend/src/components/Jogo/Jogo.js
@@ -0,0 +1,7 @@
+import "./Jogo.css";
+
+function Jogo() {
+    return <div className="jogo">Jogo</div>;
+}
+
+export default Jogo;
