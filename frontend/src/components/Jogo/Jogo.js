c94367fbe405946d6f8dc35b4a7f3959b57b4c84
brunodiascultural <brunodiascultural@gmail.com>
2022-08-23 12:24:23 -0300

    Lógica de GameOver adicionada (WIP)

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -2,7 +2,7 @@ import "./Jogo.css";
 import nuvens from "../../assets/clouds.png";
 import cano from "../../assets/pipe.png";
 import mario from "../../assets/mario.gif";
-import { useState } from "react";
+import { useRef,useState } from "react";
 
 function Jogo() {
     // Criando o estado 'estaPulando', com o valor padrão 'false',
@@ -10,18 +10,60 @@ function Jogo() {
     // Segundo valor é para atualizar o estado (SET)
     // No momneto que um estado é atualizado, o componente atualiza
     // tudo o que está sendo renderizado
-    const [estaPulando, setEstaPulando] = useState(false);
+    
+    // Criamos o estado `estaPulando`, com o valor padrão `false`.
+  // Primeiro valor é apenas para ler (GET)
+  // Segundo valor é para atualizar o estado (SET)
+  // No momento que um estado é atualizado, o componente atualiza
+  // tudo o que está sendo renderizado
+  const [estaPulando, setEstaPulando] = useState(false);
+
+  // Criamos as referências para `mario` e `cano`
+  const marioRef = useRef();
+  const canoRef = useRef();
+
+  function marioEstaNoCano() {
+    // Acessamos as referências do mario e do cano
+    const mario = marioRef.current;
+    const cano = canoRef.current;
+
+    // Se por acaso `mario` ou `cano` não forem encontrados,
+    // encerra essa função
+    if (!mario || !cano) {
+      return;
+    }
+
+    // Retorna o valor da lógica que determinar se o mário
+    // está na mesma posição do cano ou não (com as checagens
+    // que consideram toda a área do cano)
+    return (
+      cano.offsetLeft > mario.offsetLeft &&
+      cano.offsetLeft < mario.offsetLeft + mario.offsetWidth &&
+      mario.offsetTop + mario.offsetHeight > cano.offsetTop
+    );
+  }
+
+  // Implementação temporária para exibir se o mário está no cano
+  // ou não
+  setInterval(function () {
+    const valor = marioEstaNoCano();
+
+    console.log("Mário está no cano?", valor);
+  }, 100);
 
     document.onkeydown = function () {
+        // Atualizamos o estado para true
         setEstaPulando(true);
 
         setTimeout(function () { 
+            // Voltamos o estado para o valor inicial
             setEstaPulando(false);
         }, 600);
     };
-
+    // Por padrão, o elemento tem class '.mario'
     let marioClassName = "mario";
 
+    // Caso esteja pulando (valor true), a classe será '.mario' e '.mario-pulo'
     if (estaPulando) {
         marioClassName = "mario mario-pulo";
     }
