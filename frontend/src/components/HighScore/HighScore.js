1eeec6185375545d0622aabcc3ba60557142298d
brunodiascultural <brunodiascultural@gmail.com>
2022-08-31 11:23:24 -0300

    Criação da tela `HighScore`

--- frontend/src/components/HighScore/HighScore.js
+++ frontend/src/components/HighScore/HighScore.js
@@ -1,7 +1,26 @@
 import "./HighScore.css";
 
 function HighScore() {
-    return <div>HighScore</div>;
+    return <div className="HighScore">
+        <div>Você fez <b>50</b> pontos!
+        </div>
+
+        <div>
+            <h1>HighScore</h1>
+
+            <div>José - 90 pontos</div>
+            <div>João - 50 pontos</div>
+            <div>Maria - 30 pontos</div>
+        </div>
+
+        <div>
+            <h1>Registre sua pontuação</h1>
+            <form>
+                <input type="text" placeholde="Digite o seu nome..." />
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    </div>;
 }
 
 export default HighScore;
