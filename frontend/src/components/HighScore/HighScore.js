54a9c0f0223e194b85b6f440d9dbde70be805744
brunodiascultural <brunodiascultural@gmail.com>
2022-09-01 12:04:14 -0300

    Exemplo de como adicionar uma pontuação

--- frontend/src/components/HighScore/HighScore.js
+++ frontend/src/components/HighScore/HighScore.js
@@ -40,7 +40,28 @@ function HighScore(props) {
     carregarPontuacoes();
   }, []);
 
+  console.log(itens);
+
   const itensEstaoCarregando = itens === undefined;
+
+  async function salvarPontuacao(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+
+    const response = await fetch("http://localhost:3333/pontuacoes", {
+      method: "POST",
+      body: JSON.stringify({ nome: name, pontos: props.pontos }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    const body = await response.json();
+
+    console.log("Pontuação salva com sucesso:", body);
+  }
 
   return (
     <div className="HighScore">
@@ -55,8 +76,8 @@ function HighScore(props) {
           <div>Carregando...</div>
         ) : (
           <div>
-            {itens.map((item) => (
-              <div>
+            {itens.map((item, index) => (
+              <div key={`score_${index}`}>
                 {item.nome} - {item.pontos}
               </div>
             ))}
@@ -65,9 +86,9 @@ function HighScore(props) {
       </div>
 
       <div>
-        <h1>Registre sua pontuação</h1>
-        <form>
-          <input type="text" placeholde="Digite o seu nome..." />
+        <h1>Registre sua pontuação!</h1>
+        <form onSubmit={salvarPontuacao}>
+          <input type="text" name="name" placeholder="Digite o seu nome..." />
           <input type="submit" value="Enviar" />
         </form>
       </div>
